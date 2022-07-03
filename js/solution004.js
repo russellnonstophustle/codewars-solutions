e805506b6ef32eb7878fba2db69c2241466a8180
Chris Russell <crussell8690@gmail.com>
2022-07-02 19:45:13 -0600

    Add coding challenge for 7-2-2022

---
+++ js/solution004.js
@@ -0,0 +1,12 @@
+// Opposite number
+// Very simple, given an integer or a floating-point number,
+// find its opposite.
+
+function opposite(number) {
+    if (number != 0){
+      return number * -1
+    }else{
+      return number
+    }
+    
+  }
