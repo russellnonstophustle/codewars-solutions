ae9f73260d63416c95dfc71e10353e22835bda2d
Chris Russell <crussell8690@gmail.com>
2022-07-09 12:17:05 -0600

    Easy challenge using regex

---
+++ js/solution013.js
@@ -0,0 +1,7 @@
+// Remove String Spaces
+
+// Simple, remove the spaces from the string, then return the resultant string.
+// use replace() with a space globally to replace with no space 
+function noSpace(x){
+    return x.replace(/ /g, '')
+  }
