6054b0804d3b5f17256ac0f21305e62400acd897
Chris Russell <crussell8690@gmail.com>
2022-07-12 12:44:07 -0600

    Simple codewars challenge 7-12-2022

---
+++ js/solution016.js
@@ -0,0 +1,20 @@
+// Beginner Series #1 School Paperwork
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+function paperwork(n, m) {
+    // multiply classmates 'n' and pages 'm' if either is 0 return 0
+    let blankPages = (n*m);
+    if (n < 1){
+      return m*0
+    }else if (m < 1){
+      return n*0
+    }
+      return blankPages
+  }
