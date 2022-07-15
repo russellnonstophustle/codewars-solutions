4089555852f1f60380d7a630a0f6f78e43a85a76
Chris Russell <crussell8690@gmail.com>
2022-07-15 12:54:40 -0600

    Codewars challenge 7-15-2022 easy math challenge

---
+++ js/solution019.js
@@ -0,0 +1,11 @@
+// Simple Multiplication
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    // use modulus to find even or odd
+    if (number % 2 == 0){
+      return number * 8
+    }else {
+      return number * 9
+    }
+}
