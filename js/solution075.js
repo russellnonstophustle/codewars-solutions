7bfbfda43ab0fd5a8d7a31c60cdf5fa794c6dfaa
Chris Russell <crussell8690@gmail.com>
2022-09-12 11:51:34 -0600

    reduce() ternary ? operator and .length

---
+++ js/solution075.js
@@ -0,0 +1,8 @@
+// Calculate Average 
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    return array.length > 0 ? array.reduce((acc, cur) => acc + cur) / array.length : 0
+  }
