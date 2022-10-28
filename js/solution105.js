34c133c9c258222e3ebdc15f2a6806423cc46e92
Chris Russell <crussell8690@gmail.com>
2022-10-28 14:59:32 -0600

    7kyu str.split(').reverse().join(').replace(/[^a-zA-Z]g/,')

---
+++ js/solution105.js
@@ -0,0 +1,19 @@
+// Simple Fun #176: Reverse Letter
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    let reverseStr = str.split('').reverse().join('').replace(/[^a-zA-Z]/g, '')
+    console.log(reverseStr)
+    return reverseStr
+  }
