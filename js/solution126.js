c470db4233ebf8414d6b9256c6e38f917548d02e
Chris Russell <crussell8690@gmail.com>
2022-11-17 15:26:48 -0800

    Math.round() arr.join().length / arr.length arr.map() str[0].repeat(avgLength)

---
+++ js/solution126.js
@@ -0,0 +1,17 @@
+// The Average Length 
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+
+function averageLength(arr) { 
+    //   get average length -> rounded
+        const avgLength = Math.round(arr.join('').length / arr.length)
+    //     create new array using each letter the avgLetter amount 
+        return arr.map(str => str[0].repeat(avgLength))
+    }
