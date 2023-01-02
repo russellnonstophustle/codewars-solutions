67704feb0e20e29c6c5897c677f864da1fd96022
Chris Russell <crussell8690@gmail.com>
2023-01-02 09:14:02 -0800

    7kyu Row Weights

--- js/solution178.js
+++ js/solution178.js
@@ -6,7 +6,7 @@
 // Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
 
 // Notes
-// Array size is at least 1.
+// Array size is at least 1..
 
 
 function rowWeights(array){
